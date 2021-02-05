0dc4859fe6329df24c0daeb459678341034cdb77
Pendulum <auliain1799@gmail.com>
2021-02-05 15:16:05 +0700

    Menghitung Volume dan Validasinya

--- app/src/main/java/com/example/praktikum/HitungActivity.kt
+++ app/src/main/java/com/example/praktikum/HitungActivity.kt
@@ -2,10 +2,57 @@ package com.example.praktikum
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class HitungActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hitung)
+
+        val buttonHitung = findViewById<Button>(R.id.buttonHitung)
+        val editPanjang = findViewById<EditText>(R.id.editPanjang)
+        val editLebar = findViewById<EditText>(R.id.editLebar)
+        val editTinggi = findViewById<EditText>(R.id.editTinggi)
+        val textHasil = findViewById<TextView>(R.id.textHasil)
+
+
+        buttonHitung.setOnClickListener{
+            var isEmptyField = false
+
+            val stringPanjang =editPanjang.text.toString().trim()
+            val stringLebar =editLebar.text.toString().trim()
+            val stringTinggi =editTinggi.text.toString().trim()
+
+            if (stringPanjang.equals("")){
+                isEmptyField = true
+                editPanjang.error = "Data kosong"
+            }
+            if (stringLebar.isEmpty()){
+                isEmptyField = true
+                editLebar.error = "Data kosong"
+
+            }
+            if (stringTinggi == ""){
+                isEmptyField = true
+                editTinggi.error = "Data kosong"
+
+            }
+            if (!isEmptyField){
+
+                val panjang = stringPanjang.toFloat()
+                val lebar = stringLebar.toFloat()
+                val tinggi = stringTinggi.toFloat()
+
+                textHasil.text = (panjang * lebar * tinggi).toString()
+
+            }
+
+
+
+
+        }
     }
 }
