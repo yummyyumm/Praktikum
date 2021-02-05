d327b67d81d689d1765b6b48611a468ef531eca9
Pendulum <auliain1799@gmail.com>
2021-02-05 14:01:00 +0700

    Implemetasi Toast onClick TextView

--- app/src/main/java/com/example/praktikum/MainActivity.kt
+++ app/src/main/java/com/example/praktikum/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.praktikum
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView1 = findViewById<TextView>(R.id.textView1)
+
+        textView1.setOnClickListener(View.OnClickListener {
+            Toast.makeText(this, "hai", Toast.LENGTH_LONG).show()
+        })
     }
 }
